2db7554b3414dfc517daf0efb43c673314f382d1
Sam Barnham <barnham.s@etoncollege.org.uk>
2019-11-05 12:18:02 +0000

    Initial commit of project files

---
+++ Standard Algorithms/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Standard Algorithms
+//
+//  Created by Barnham, Samuel (ABH) on 05/11/2019.
+//  Copyright © 2019 Barnham, Samuel (ABH). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Sam"
+print(name)
+
+var i = 1
+while i != 20 {
+    i = i + 1
+    print(i)
+}
